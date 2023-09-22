421d7e9b8312be476dacc1fa289109979d42cd5d
Tiana Lopez <tianadavin@gmail.com>
2023-09-21 20:06:37 -0700

    Completes fetch and append

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+
+// Global variables
+const ramenMenu = document.querySelector('#ramen-menu')
+
+//function display ramens in ramen menu
+const appendRamen = (ramenObj) => {
+  const ramenDiv = document.createElement('img')
+  ramenDiv.src = ramenObj.image
+
+  ramenMenu.append(ramenDiv)
+}
+
+
+//fetch function to load ramen
+const loadRamen = () => {
+  fetch('http://localhost:3000/ramens')
+  .then(resp => resp.json())
+  .then((ramenArray) => {
+    ramenArray.forEach((ramenObj) => appendRamen(ramenObj))})
+}
+
+
+//call functions
+loadRamen();
+})
